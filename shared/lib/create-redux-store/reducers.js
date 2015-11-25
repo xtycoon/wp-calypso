e12afff9280901cca68a62c3fe8e7fd945c916fe
Andrew Duthie <andrew@andrewduthie.com>
2015-11-25 10:02:29 -0500

    Sites: Include sites reducers in global reducer

--- shared/lib/create-redux-store/reducers.js
+++ shared/lib/create-redux-store/reducers.js
@@ -7,7 +7,9 @@ import { combineReducers } from 'redux';
  * Internal dependencies
  */
 import sharing from 'lib/sharing/reducers';
+import sites from 'lib/sites/reducers';
 
 export default combineReducers( {
-	sharing
+	sharing,
+	sites
 } );
