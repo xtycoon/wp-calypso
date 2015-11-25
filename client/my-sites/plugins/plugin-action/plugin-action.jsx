b9af8cafda41c804d0fbe4e13980a75e3ac507b1
Enej Bajgoric <enej.bajgoric@gmail.com>
2015-11-25 12:00:56 -0800

    Plugin Action: Refactored so that plugins that pass down children are still able to show the info-popover

--- client/my-sites/plugins/plugin-action/plugin-action.jsx
+++ client/my-sites/plugins/plugin-action/plugin-action.jsx
@@ -22,14 +22,14 @@ module.exports = React.createClass( {
 		}
 	},
 
-	renderLabel: function( id ) {
+	renderLabel: function() {
 		if ( this.props.label ) {
 			return (
 				<label
 					className="plugin-action__label"
 					ref="disabledInfoLabel"
 					onClick={ this.handleAction }
-					htmlFor={ id }
+					htmlFor={ this.props.htmlFor }
 					>
 					{ this.props.label }
 				</label>
@@ -38,23 +38,24 @@ module.exports = React.createClass( {
 		return null;
 	},
 
+	renderDisabledInfo: function( id ) {
+		return (
+			<div className="plugin-action__disabled-info">
+				<InfoPopover
+					position="bottom left"
+					popoverName={ 'Plugin Action Disabled' + this.props.label }
+					gaEventCategory="Plugins"
+					ref="infoPopover"
+					ignoreContext={ this.refs && this.refs.disabledInfoLabel }
+					>
+					{ this.props.disabledInfo }
+				</InfoPopover>
+				{ this.renderLabel( id ) }
+			</div>
+		);
+	},
+
 	renderToggle: function( id ) {
-		if ( this.props.disabledInfo ) {
-			return (
-				<div className="plugin-action__disabled-info">
-					<InfoPopover
-						position="bottom left"
-						popoverName={ 'Plugin Action Disabled' + this.props.label }
-						gaEventCategory="Plugins"
-						ref="infoPopover"
-						ignoreContext={ this.refs && this.refs.disabledInfoLabel }
-						>
-						{ this.props.disabledInfo }
-					</InfoPopover>
-					{ this.renderLabel( id ) }
-				</div>
-			);
-		}
 		return (
 			<CompactToggle
 				onChange={ this.props.action }
@@ -77,14 +78,22 @@ module.exports = React.createClass( {
 		);
 	},
 
-	render: function() {
+	renderInner: function() {
 		var id = this.props.htmlFor;
+		if ( this.props.disabledInfo ) {
+			return this.renderDisabledInfo( id );
+		}
+		return (
+		0 < React.Children.count( this.props.children )
+			? this.renderChildren( id )
+			: this.renderToggle( id )
+		);
+	},
+
+	render: function() {
 		return (
 			<div className={ classNames( 'plugin-action', this.props.className ) }>
-				{ 0 < React.Children.count( this.props.children )
-					? this.renderChildren( id )
-					: this.renderToggle( id )
-				}
+				{ this.renderInner() }
 			</div>
 		);
 	}
