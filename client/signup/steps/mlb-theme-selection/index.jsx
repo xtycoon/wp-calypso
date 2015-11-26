3c955c91593995f80bad51905a6a70dcd03c904c
oskosk <oskosk@gmail.com>
2015-11-26 09:55:08 -0300

    Localize text on header and terms acceptance text/link

--- client/signup/steps/mlb-theme-selection/index.jsx
+++ client/signup/steps/mlb-theme-selection/index.jsx
@@ -85,10 +85,13 @@ module.exports = React.createClass( {
 					}.bind( this ) ) }
 				</div>
 				<div htmlClass="mlb-themes__terms">
-					<h2>MLB.com/blogs Rules</h2>
+					<h2>{ this.translate( 'MLB.com/blogs Rules') } </h2>
 					<p>
-						By clicking continue, you understand that activating an MLB.com/blogs account indicates your acceptance of the
-						&nbsp;<a href="http://mlb.mlb.com/mlb/official_info/about_mlb_com/terms_of_use.jsp" target="_blank">Terms of Use</a>.
+						{ this.translate( 'By clicking continue, you understand that activating an MLB.com/blogs account indicates your acceptance of the {{a}}Terms of Use{{/a}}', {
+							components: {
+								a: <a href="http://mlb.mlb.com/mlb/official_info/about_mlb_com/terms_of_use.jsp" target="_blank" />
+							}
+						} ) }
 					</p>
 				</div>
 			</div>
